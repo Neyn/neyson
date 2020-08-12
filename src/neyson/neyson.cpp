779c1fd5a2d696b66b366d7ac27c00a5e19abdc9
Shahriar <shahriar25.ss@gmail.com>
2020-08-13 02:17:22 +0430

    Added license to sources and fixed escape handing

--- src/neyson/neyson.cpp
+++ src/neyson/neyson.cpp
@@ -1,3 +1,35 @@
+/*
+  BSD 3-Clause License
+
+  Copyright (c) 2020, Shahriar Rezghi <shahriar25.ss@gmail.com>
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its
+     contributors may be used to endorse or promote products derived from
+     this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "neyson.h"
 
 #include <cmath>
@@ -162,21 +194,20 @@ bool fixString(String &string, const char *ptr, size_t len)
             else if (ptr[i] == 'u')
             {
                 uint32_t code = 0;
-                size_t end = ++i + 4;
-                if (end > len) return false;
-
-                for (; i < end; ++i)
+                if (++i + 4 > len) return false;
+                for (int j = 0; j < 4; ++i, ++j)
                 {
-                    (code <<= 4) += uint32_t(ptr[i]);
+                    code <<= 4;
                     if (ptr[i] >= '0' && ptr[i] <= '9')
-                        code -= '0';
+                        code += ptr[i] - '0';
                     else if (ptr[i] >= 'a' && ptr[i] <= 'f')
-                        code -= 'a' - 10;
+                        code += ptr[i] - 'a' + 10;
                     else if (ptr[i] >= 'A' && ptr[i] <= 'F')
-                        code -= 'A' - 10;
+                        code += ptr[i] - 'A' + 10;
                     else
                         return false;
                 }
+                --i;
 
                 if (code <= 0x7F)
                     string[j++] = char(code);
@@ -336,6 +367,14 @@ String unfixString(const String &string)
             output.push_back('r');
         else if (string[i] == '\t')
             output.push_back('t');
+        else if (string[i] < 10)
+            output.append("u000").push_back('0' + string[i]);
+        else if (string[i] < 16)
+            output.append("u000").push_back('A' + string[i] - 10);
+        else if (string[i] < 26)
+            output.append("u001").push_back('0' + string[i] - 16);
+        else if (string[i] < 32)
+            output.append("u001").push_back('A' + string[i] - 26);
         else
             output.back() = string[i];
     }
--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,35 @@
+/*
+  BSD 3-Clause License
+
+  Copyright (c) 2020, Shahriar Rezghi <shahriar25.ss@gmail.com>
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its
+     contributors may be used to endorse or promote products derived from
+     this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <gtest/gtest.h>
 
 #include <cmath>
@@ -85,12 +117,8 @@ Value randomValue();
 
 string randomString()
 {
-    string data = ".-+()[]{}!@#$%^&*\r\n\t\b\f\\/\"`'";
-    for (auto i = 'a'; i <= 'z'; ++i) data += i;
-    for (auto i = 'A'; i <= 'Z'; ++i) data += i;
-    for (auto i = '0'; i <= '9'; ++i) data += i;
     string str(rand() % 10, 0);
-    for (auto &c : str) c = data[rand() % data.size()];
+    for (auto &c : str) c = rand() % 128;
     return str;
 }
 
@@ -326,7 +354,7 @@ TEST(Neyson, Random)
         auto S = IO::write(E);
         Parse;
 
-        if (!R)
+        if (false)
         {
             for (size_t i = 0; i < 80; ++i) cout << '-';
             cout << endl;
@@ -339,7 +367,7 @@ TEST(Neyson, Random)
 
 int main(int argc, char **argv)
 {
-    ::testing::InitGoogleTest(&argc, argv);
     srand(static_cast<unsigned int>(time(nullptr)));
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
